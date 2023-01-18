d5cb651f1e9aa3499d1b6e0c65f5aa75a09d90f5
ItzMari0 <msajr@outlook.com>
2023-01-18 16:30:44 -0300

    Classes criadas, importação/exportação no index.ts. Instâncias pendentes

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,14 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
